8ed58fcd27b71d32544a533cf4f7787c0d3c6421
toshokan <toshokan@shojigate.net>
2020-12-29 03:02:54 -0500

    Add more tests

--- flag-mast/src/lib.rs
+++ flag-mast/src/lib.rs
@@ -18,4 +18,57 @@ mod tests {
 	dog.set_sits(true);
 	assert!(dog.can_bark() == true);
     }
+
+    const BLUE: u8 = 0x1;
+    const RED: u8 = 0x2;
+
+    mod secondary_colours {
+	pub const YELLOW: u16 = 0x4;
+    }
+
+    #[test]
+    fn it_works_two() {
+	#[derive(Flags, Default)]
+	#[flag_debug]
+	#[flag(name = "blue", value = "BLUE")]
+	#[flag(name = "red", value = "RED")]
+	#[flag(name = "yellow", value = "secondary_colours::YELLOW")]
+	#[flag(name = "purple", value = "BLUE | RED")]
+	#[flag(name = "black", value = 16)]
+	#[repr(C)]
+	struct Colour{
+	    is_useful: bool,
+	    #[flag_backing_field] 
+	    flags: u16
+	}
+
+	
+	let mut colour = Colour::default();
+	colour.set_blue(true);
+	colour.set_red(true);
+	
+	dbg!(&colour);
+	assert!(colour.purple());
+    }
+
+    #[test]
+    fn it_works_three() {
+	#[derive(Flags, Default)]
+	#[flag_debug(compact)]
+	#[flag(name = "one", value = 4)]
+	#[flag(name = "second", method_name = "two", value = 8)]
+	#[flag(name = "three", value = 16)]
+	struct Buttons(#[flag_backing_field] u16);
+
+	let mut buttons = Buttons::default();
+	buttons.set_one(true);
+	buttons.set_three(true);
+	
+	println!("{:?}", buttons);
+	println!("---");
+	println!("{:#?}", buttons);
+    }
+
 }
+
+
